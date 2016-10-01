ec79251ea8f2b7b51c8c88b951595e2ae684a967
glory <glory.angelova@gmail.com>
2016-10-01 15:45:09 +0200

    small changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,30 +1,33 @@
 $(document).ready(function(){
   $("#nav_bar_toggle").on("click", function(){
     $("#main_nav_bar").toggleClass("open");
-    
+
+
+    // ASCENSOR plugin
+
+    var ascensor = $('#content').ascensor({direction:"x"});
+    var ascensorInstance = $('#content').data('ascensor');
+
+          $(".links-to-floor li").click(function(event, index) {
+            ascensorInstance.scrollToFloor($(this).index());
+          });
+
+          $(".links-to-floor li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
+
+
+          $('#content').ascensor({
+            AscensorName:'ascensor',
+             ChildType:'div',
+             AscensorFloorName:'Home | Implementation | HTML | Jquery | CSS | Smartphone | End',
+             Time:1000,
+             Easing:'easeInOutCubic',
+             WindowsOn:1,
+             Direction:'chocolate',
+             AscensorMap:'1|1 & 1|2 & 2|2 & 1|3 & 1|4 & 2|4 & 1|5',
+             KeyNavigation: true,
+             Queued:false,
+             QueuedDirection:"x"
+          });
+
   });
 });
-  // ASCENSOR plugin
-  var ascensor = $('#content').ascensor({direction:"x"});
-  var ascensorInstance = $('#content').data('ascensor');
-
-  			$(".links-to-floor li").click(function(event, index) {
-  				ascensorInstance.scrollToFloor($(this).index());
-  			});
-
-  			$(".links-to-floor li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
-
-
-        $('#content').ascensor({
-          AscensorName:'ascensor',
-           ChildType:'div',
-           AscensorFloorName:'Home | Implementation | HTML | Jquery | CSS | Smartphone | End',
-           Time:1000,
-           Easing:'easeInOutCubic',
-           WindowsOn:1,
-           Direction:'chocolate',
-           AscensorMap:'1|1 & 1|2 & 2|2 & 1|3 & 1|4 & 2|4 & 1|5',
-           KeyNavigation: true,
-           Queued:false,
-           QueuedDirection:"x"
-        });
