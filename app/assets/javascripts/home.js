255594289f328edf902c34a9d91b8a420960d716
glory <glory.angelova@gmail.com>
2016-10-11 15:08:10 +0200

    add translation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
   $("#nav_bar_toggle").on("click", function(){
     $("#main_nav_bar").toggleClass("open");
-    alert("glory");
+    
 
-    $('#main_nav_bar ul li a').on('click', function(){
-     $(this).addClass('current');
-    });
   });
 });
